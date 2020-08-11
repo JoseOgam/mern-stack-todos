08ba7ba84e550344cfe1076ad5bbd97750b2f529
JoseOgam <jotienoochieng@gmail.com>
2020-08-11 19:15:17 -0400

    implementing axios get method

--- backend/db/router.js
+++ backend/db/router.js
@@ -1,25 +1,25 @@
-var express = require('express')
-var router = express.Router()
-var Todo = require('./models')
+var express = require("express");
+var router = express.Router();
+var Todo = require("./models");
 
-var Todo = require('./models')
-router.post('/todos', async (req, res) => {
-  var todo = new Todo(req.body)
+var Todo = require("./models");
+router.post("/todos", async (req, res) => {
+  var todo = new Todo(req.body);
   try {
-    await todo.save()
-    res.status(201).send(todo)
+    await todo.save();
+    res.status(201).send(todo);
   } catch (e) {
-    res.status(400).send(e)
+    res.status(400).send(e);
   }
-})
+});
 
-router.get('/todos', async (req, res) => {
+router.get("/todos", async (req, res) => {
   try {
-    var todo = await Todo.find({})
-    res.send(todo)
+    var todo = await Todo.find({});
+    res.send(todo);
   } catch (e) {
-    res.status(400).send(e)
+    res.status(400).send(e);
   }
-})
+});
 
-module.exports = router
+module.exports = router;
--- src/components/TodosLists.js
+++ src/components/TodosLists.js
@@ -1,13 +1,40 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 class TodoList extends React.Component {
-  render () {
+  constructor(props) {
+    super(props);
+    this.state = { todos: [] };
+  }
+  componentDidMount() {
+    axios
+      .get("http://localhost:4000/todos/")
+      .then((response) => {
+        this.setState({ todos: response.data });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+  render() {
     return (
       <div>
-        <p>Welcome to Todos List Component!!</p>
+        <h3>Todos List</h3>
+        <table className="table table-striped" style={{ marginTop: 20 }}>
+          <thead>
+            <tr>
+              <th>Description</th>
+              <th>Responsible</th>
+              <th>Priority</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>{/* {this.todoList()} */}</tbody>
+        </table>
       </div>
-    )
+    );
   }
 }
 
-export default TodoList
+export default TodoList;
